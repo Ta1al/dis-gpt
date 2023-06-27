95e03d0dc1e0ae2e03d5816cfae6b460a49c2e13
Talal <talalahmad257@gmail.com>
2023-06-27 06:48:26 +0000

    Create thread

--- src/commands/prompt.ts
+++ src/commands/prompt.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, SlashCommandBuilder, TextChannel } from "discord.js";
 import { ChatGPTUnofficialProxyAPI, ChatMessage } from "chatgpt";
 export default {
   data: new SlashCommandBuilder()
@@ -11,22 +11,55 @@ export default {
       apiReverseProxyUrl: "https://api.pawan.krd/backend-api/conversation",
       debug: true
     });
-    await interaction.deferReply();
     let partial: ChatMessage | undefined = undefined;
+    if (
+      !interaction.channel?.isTextBased() ||
+      interaction.channel?.isDMBased() ||
+      interaction.channel?.isThread()
+    ) {
+      return interaction.reply({
+        content: "⚠ This command can only be used in a guild text channel.",
+        ephemeral: true
+      });
+    }
 
+    await interaction.deferReply({ ephemeral: true });
+    const channel = interaction.channel as TextChannel,
+      thread = await channel.threads
+        .create({
+          name: interaction.user.username
+        })
+        .catch(() => {
+          interaction.editReply({
+            content: "⚠ Failed to create thread."
+          });
+          return undefined;
+        });
+    if (!thread) return;
+    const txt =
+        `${interaction.user.toString()}\n` +
+        `Prompt: ${interaction.options.get("prompt")!.value as string}`,
+      msg = await thread.send(txt).catch(() => {
+        interaction.editReply({
+          content: "⚠ Failed to send message in thread."
+        });
+        return undefined;
+      });
+    if (!msg) return;
     const temp = setInterval(() => {
-      if (partial) {
-        interaction.editReply(partial.text);
-        partial = undefined;
-      }
-    }, 1500);
-    const res = await api.sendMessage(interaction.options.get("prompt")!.value as string, {
-      onProgress: progress => {
-        partial = progress;
-      }
-    });
+        if (partial) {
+          msg.edit(txt + "\n\n**Response:** " + partial.text).catch();
+          partial = undefined;
+        }
+      }, 1500),
+      res = await api.sendMessage(interaction.options.get("prompt")!.value as string, {
+        onProgress: progress => {
+          partial = progress;
+        },
+        timeoutMs: 2 * 60 * 1000
+      });
     clearInterval(temp);
-    await interaction.editReply(res.text);
+    await msg.edit(txt + "\n\n**Response:** " + res.text);
   }
 };
 
